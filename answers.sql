8c529a584a70255b27ccafff7398944e0e17b905
ian_ndiga <ianndiga0@gmail.com>
2024-09-11 20:13:23 +0300

    answers.sql

--- answers.sql
+++ answers.sql
@@ -61,3 +61,58 @@
 --          discharge_disposition VARCHAR(255) NOT NULL
 -- );
 
+
+
+-- USE hospital_db
+-- SELECT first_name, last_name, date_of_birth
+-- FROM patients;
+
+-- SELECT provider_id, first_name, provider_speciality
+-- FROM providers;
+-- SELECT *
+-- FROM patients
+-- WHERE first_name LIKE 'AB%';
+
+-- SELECT *
+-- FROM providers
+-- WHERE provider_speciality LIKE '%Y';
+
+-- SELECT *
+-- FROM patients
+-- WHERE date_of_birth > '1980-01-01';
+
+-- SELECT *
+-- FROM ed_visits
+-- WHERE acuity >= 2;
+
+-- SELECT *
+-- FROM patients
+-- WHERE language= 'spanish';
+
+-- SELECT *
+-- FROM ed_visits
+-- WHERE reason_for_visit = 'Migraine'
+--   AND disposition = 'Admitted';
+
+-- SELECT *
+-- FROM patients
+-- WHERE date_of_birth BETWEEN '1975-01-01' AND '1980-12-31';
+
+-- SELECT *
+-- FROM patients
+-- ORDER BY last_name ASC;
+
+-- SELECT *
+-- FROM visits
+-- ORDER BY date_of_visit DESC;
+
+-- SELECT *
+-- FROM admissions
+-- WHERE primary_diagnosis = 'Stroke'
+-- AND discharge_dispostion = 'Home';
+
+-- SELECT *
+-- FROM providers
+-- WHERE date_joined > '1995-12-31'
+-- AND provider_speciality IN ('Pediatrics', 'Cardiology');
+
